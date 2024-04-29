b69bfb1a74666f434b4f6b7483df5ffc03612c0b
Va-leria <leraalukova@gmail.com>
2024-04-29 21:18:58 +0300

    Add all for rules

--- server/routes/main.js
+++ server/routes/main.js
@@ -6,10 +6,19 @@ const router = express.Router();
 router.get('', (req, res) => {
     const locals = {
         title: "Графикус",
-        styles: ["/css/reset.css", "/css/styles.css"]
+        styles: ["/css/reset.css", "/css/styles.css", "/css/header.css"]
     }
 
     res.render('index', { locals });
+});
+
+router.get('/rules', (req, res) => {
+    const locals = {
+        title: "Правила",
+        styles: ["/css/reset.css", "/css/rules_styles.css", "/css/header.css", "/css/footer.css" ]
+    }
+
+    res.render('rules', { locals });
 });
 
 module.exports = router;
